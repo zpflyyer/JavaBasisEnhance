24fa3b16b57014769d9b0ba8f18ac437e7db82b5
pengfei.zhao <pengfei.zhao@163.com>
2019-11-03 23:35:33 +0800

    【红黑树】插入再平衡

--- DSAndAlgorithm/Tree/src/main/java/com/javabase/dsandalgo/tree/binary/search/balanced/EasyRedBlackTree.java
+++ DSAndAlgorithm/Tree/src/main/java/com/javabase/dsandalgo/tree/binary/search/balanced/EasyRedBlackTree.java
@@ -13,6 +13,8 @@ public class EasyRedBlackTree<T extends Comparable<? super T>> {
     //红黑树初始状态:一个黑色的NIL节点
     private RedBlackTreeNode<T> root = NIL;
 
+    private RedBlackTreeNode<T> recentInsertedNode = NIL;
+
     /**
      * 插入算法
      */
@@ -23,6 +25,7 @@ public class EasyRedBlackTree<T extends Comparable<? super T>> {
 
     public void insert(T toBeInserted) {
         this.root = insert(NIL, this.root, toBeInserted);
+        this.afterInsert();
     }
 
     private RedBlackTreeNode<T> insert(RedBlackTreeNode<T> parent, RedBlackTreeNode<T> current, T tobeInserted) {
@@ -30,6 +33,7 @@ public class EasyRedBlackTree<T extends Comparable<? super T>> {
         if (current.equals(NIL)) {
             current = this.newNodeFor(tobeInserted, parent);
             current.parent = parent;
+            this.recentInsertedNode = current;
         } else {
             int compareResult = current.getElement().compareTo(tobeInserted);
             if (compareResult > 0) {
@@ -39,6 +43,103 @@ public class EasyRedBlackTree<T extends Comparable<? super T>> {
             }
         }
         return current;
+    }
+
+    // re-balance function
+    private void afterInsert() {
+        if (!this.recentInsertedNode.equals(NIL)) {
+            RedBlackTreeNode<T> z = this.recentInsertedNode;
+            //如果z的父亲p是红色，说明z的的祖父g是黑色，且z的祖父g高度<=根的高度
+            //如果z的父亲p是黑色，那么p有可能是NIL
+            while (z.parent.color == COLOR.RED) {
+                RedBlackTreeNode<T> p = z.parent;
+                RedBlackTreeNode<T> g = p.parent;
+                if (g.left.equals(p)) {
+                    if (g.right.color == COLOR.RED) {
+                        p.color = COLOR.BLACK;
+                        g.right.color = COLOR.BLACK;
+                        g.color = COLOR.RED;
+                        z = g;
+                    } else {
+                        if (z == p.right) {
+                            this.leftRotate(p);
+                            p = g.left;
+                            z = p.left;
+                        }
+                        g.color = COLOR.RED;
+                        p.color = COLOR.BLACK;
+                        this.rightRotate(g);
+                        //结束时需要修复根节点的引用
+                        if (p.parent == NIL) {
+                            this.root = p;
+                        }
+                    }
+                } else {
+                    if (g.left.color == COLOR.RED) {
+                        p.color = COLOR.BLACK;
+                        g.left.color = COLOR.BLACK;
+                        g.color = COLOR.RED;
+                        z = g;
+                    } else {
+                        if (z == p.left) {
+                            this.rightRotate(p);
+                            p = g.right;
+                            z = p.right;
+                        }
+                        g.color = COLOR.RED;
+                        p.color = COLOR.BLACK;
+                        this.leftRotate(g);
+                        //结束时需要修复根节点的引用
+                        if (p.parent == NIL) {
+                            this.root = p;
+                        }
+                    }
+                }
+            }
+            // don't forget
+            this.root.color = COLOR.BLACK;
+        }
+    }
+
+    private void leftRotate(RedBlackTreeNode<T> p) {
+        RedBlackTreeNode<T> g = p.parent;
+        RedBlackTreeNode<T> z = p.right;
+        RedBlackTreeNode<T> b = z.left;
+        p.right = b;
+        if (b != NIL) {
+            b.parent = p;
+        }
+
+        p.parent = z;
+        z.left = p;
+
+        z.parent = g;
+        if (g.left == p) {
+            g.left = z;
+        } else {
+            g.right = z;
+        }
+    }
+
+    private void rightRotate(RedBlackTreeNode<T> p) {
+        RedBlackTreeNode<T> g = p.parent;
+        RedBlackTreeNode<T> z = p.left;
+        RedBlackTreeNode<T> b = z.right;
+
+        p.left = b;
+        if (b != NIL) {
+            b.parent = p;
+        }
+
+        p.parent = z;
+        z.right = p;
+
+        z.parent = g;
+        if (g.left == p) {
+            g.left = z;
+        } else {
+            g.right = z;
+        }
     }
 
     /**
@@ -96,7 +197,7 @@ public class EasyRedBlackTree<T extends Comparable<? super T>> {
 
     public RedBlackTreeNode<T> findPredecessor(RedBlackTreeNode<T> current) {
         //NIL没有前置和后继节点
-        if (current.equals(NIL)){
+        if (current.equals(NIL)) {
             return NIL;
         }
 
@@ -116,17 +217,17 @@ public class EasyRedBlackTree<T extends Comparable<? super T>> {
         return parent;
     }
 
-    public RedBlackTreeNode<T>  findSuccessor(RedBlackTreeNode<T> current) {
-        if (current.equals(NIL)){
+    public RedBlackTreeNode<T> findSuccessor(RedBlackTreeNode<T> current) {
+        if (current.equals(NIL)) {
             return NIL;
         }
 
-        if (!current.getRight().equals(NIL)){
+        if (!current.getRight().equals(NIL)) {
             return this.findMin(current.getRight());
         }
 
         RedBlackTreeNode<T> parent = current.getParent();
-        while (!parent.equals(NIL) && !parent.getLeft().equals(current)){
+        while (!parent.equals(NIL) && !parent.getLeft().equals(current)) {
             current = parent;
             parent = parent.getParent();
         }
