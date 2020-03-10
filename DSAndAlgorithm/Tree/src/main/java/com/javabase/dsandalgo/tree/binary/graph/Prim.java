f0ff8e094bae00b5aaaa394fc9311c4a2bf1a8ca
pengfei.zhao <pengfei.zhao@163.com>
2020-03-10 16:09:52 +0800

    prim算法最小生成树维护

--- DSAndAlgorithm/Tree/src/main/java/com/javabase/dsandalgo/tree/binary/graph/Prim.java
+++ DSAndAlgorithm/Tree/src/main/java/com/javabase/dsandalgo/tree/binary/graph/Prim.java
@@ -1,5 +1,7 @@
 package com.javabase.dsandalgo.tree.binary.graph;
 
+import com.alibaba.fastjson.JSON;
+
 import java.util.*;
 
 public class Prim {
@@ -7,25 +9,31 @@ public class Prim {
     public static void main(String[] args) {
         Graph graph = new Graph(new Scanner(System.in));
 
-        QueueNode mst = null;
-
+        QueueNode mst = new QueueNode(0, 0);
+        Map<Integer, QueueNode> vertex2QueueNode = new HashMap<>();
         // insert all vertex into priority Queue with infinite priority
         PriorityQueue<QueueNode> queue = new PriorityQueue<>(Comparator.comparingInt(QueueNode::getPriority));
-        for (int i = 0; i < graph.getV(); i++) {
+        queue.add(mst);
+        for (int i = 1; i < graph.getV(); i++) {
             queue.add(new QueueNode(i, Integer.MAX_VALUE));
         }
 
         // prim greedy choice
         while (!queue.isEmpty()) {
             QueueNode min = queue.remove();
-            if (mst == null) {
-                mst = min;
+            vertex2QueueNode.put(min.vertex, min);
+            // prim算法当节点被出队时，其父亲必然是已经最终确定了的
+            if (!min.equals(mst)) {
+                vertex2QueueNode.keySet().stream()
+                        .filter(vertex -> vertex.equals(min.parent.vertex))
+                        .findFirst()
+                        .ifPresent(parentVertex -> vertex2QueueNode.get(parentVertex).getChildren().add(min));
             }
             graph.getAdj()[min.vertex].forEach((v, weight) -> {
                 // 仅更新在V-A里的节点的优先级
                 Optional<QueueNode> nodeOptional = queue.stream().filter(node -> node.getVertex() == v).findFirst();
                 nodeOptional.ifPresent(node -> {
-                    // 更新时需要注意：在prim算法过程中，一个节点在未取出之前，其优先级和其MST父节点可能会被更新多次
+                    // 更新时需要注意：在prim算法过程中，一个节点在未取出之前，其优先级和其MST父节点可能会被更新多次，以最短的为准
                     if (weight < node.getPriority()) {
                         node.setPriority(weight);
                         node.setParent(min);
@@ -35,12 +43,14 @@ public class Prim {
                 });
             });
         }
+        System.out.println(JSON.toJSONString(mst));
     }
 
     private static final class QueueNode {
         private int vertex;
         private int priority;
         private QueueNode parent;
+        private List<QueueNode> children = new ArrayList<>();
 
         public QueueNode(int v, int priority) {
             this.vertex = v;
@@ -83,12 +93,21 @@ public class Prim {
             return this;
         }
 
+        public List<QueueNode> getChildren() {
+            return children;
+        }
+
+        public QueueNode setChildren(List<QueueNode> children) {
+            this.children = children;
+            return this;
+        }
+
         @Override
         public String toString() {
             return "QueueNode{" +
                     "vertex=" + vertex +
                     ", priority=" + priority +
-                    ", parent=" + parent +
+                    ", children=" + children +
                     '}';
         }
     }
