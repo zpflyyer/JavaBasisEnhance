895d6c930872a8c58264698c5520faba31a9e97e
pengfei.zhao <pengfei.zhao@163.com>
2020-03-14 18:14:17 +0800

    二叉树带前驱/后继的遍历

--- DSAndAlgorithm/src/main/java/com/javabase/dsandalgo/tree/binary/leetcode/bfsanddfs/IncreasingOrderSearchTree897.java
+++ DSAndAlgorithm/src/main/java/com/javabase/dsandalgo/tree/binary/leetcode/bfsanddfs/IncreasingOrderSearchTree897.java
@@ -1,32 +1,67 @@
 package com.javabase.dsandalgo.tree.binary.leetcode.bfsanddfs;
 
+import com.javabase.dsandalgo.tree.binary.common.TreeNode;
+
 public class IncreasingOrderSearchTree897 {
 
     public static void main(String[] args) {
-
+        IncreasingOrderSearchTree897 inc = new IncreasingOrderSearchTree897();
+        Integer[] array = {5, 3, 6, 2, 4, null, 8, 1, null, null, null, null, null, 7, 9};
+        TreeNode root = TreeNode.fromIntegerArray(array);
+        System.out.println(inc.increasingBST(root));
     }
 
-    public TreeNode increasingBST(TreeNode root) {
+    private TreeNode increasingBST(TreeNode root) {
+        return this.dfs(root, null);
+    }
+
+    /**
+     * @param root 一棵子树
+     * @param next 中序遍历里，以root为根的子树的后继节点
+     * @return 顺序化后的新树的根节点
+     */
+    // 右链，递增
+    private TreeNode dfs(TreeNode root, TreeNode next) {
+        if (root == null) {
+            return next;
+        }
+        TreeNode head = this.dfs(root.left, root);
+        root.left = null;
+        root.right = this.dfs(root.right, next);
+        return head;
+    }
+
+    // 左链，递增
+    private TreeNode dfs1(TreeNode root, TreeNode next) {
         if (root != null) {
-            if (root.left != null) {
-                TreeNode left = this.increasingBST(root.left);
-                left.right = root;
-            }
-            if (root.right != null) {
-                root.right = this.increasingBST(root.right);
-            }
+            TreeNode head = this.dfs1(root.left, root);
+            root.left = this.dfs1(root.right, next);
+            root.right = null;
+            return head;
         }
-        return root;
+        return next;
     }
 
-    class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode(int x) {
-            val = x;
+    // 左链，递减
+    private TreeNode dfs2(TreeNode root, TreeNode next) {
+        if (root != null) {
+            TreeNode head = this.dfs2(root.right, root);
+            root.right = null;
+            root.left = this.dfs2(root.left, next);
+            return head;
         }
+        return next;
+    }
+
+    // 右链，递减
+    private TreeNode dfs3(TreeNode root, TreeNode next) {
+        if (root != null) {
+            TreeNode head = this.dfs3(root.right, root);
+            root.right = this.dfs3(root.left, next);
+            root.left = null;
+            return head;
+        }
+        return next;
     }
 
 }
---
+++ DSAndAlgorithm/src/main/java/com/javabase/dsandalgo/tree/binary/leetcode/bst/BinarySearchTreeToGreaterSumTree1038.java
@@ -0,0 +1,47 @@
+package com.javabase.dsandalgo.tree.binary.leetcode.bst;
+
+import com.alibaba.fastjson.JSON;
+import com.javabase.dsandalgo.tree.binary.common.TreeNode;
+
+public class BinarySearchTreeToGreaterSumTree1038 {
+
+    public static void main(String[] args) {
+        Integer[] array = {4, 1, 6, 0, 2, 5, 7, null, null, null, 3, null, null, null, 8};
+        TreeNode root = TreeNode.fromIntegerArray(array);
+        BinarySearchTreeToGreaterSumTree1038 bsttgst = new BinarySearchTreeToGreaterSumTree1038();
+        bsttgst.inverseInOrder(root);
+        System.out.println(JSON.toJSONString(bsttgst.bstToGst(root)));
+    }
+
+    public TreeNode bstToGst(TreeNode root) {
+        this.inverseInOrder(root, 0);
+        return root;
+    }
+
+    private int inverseInOrder(TreeNode root, int sum) {
+        if (root == null) {
+            return sum;
+        }
+        int rightSum = this.inverseInOrder(root.right, sum);
+        root.val = root.val + rightSum;
+        return this.inverseInOrder(root.left, root.val);
+    }
+
+    private int inOrder(TreeNode root, int sum) {
+        if (root != null) {
+            int leftSum = this.inOrder(root.left, sum);
+            root.val = root.val + leftSum;
+            return this.inOrder(root.right, root.val);
+        }
+        return sum;
+    }
+
+    private void inverseInOrder(TreeNode root) {
+        if (root != null) {
+            this.inverseInOrder(root.right);
+            System.out.println(root.val);
+            this.inverseInOrder(root.left);
+        }
+    }
+
+}
