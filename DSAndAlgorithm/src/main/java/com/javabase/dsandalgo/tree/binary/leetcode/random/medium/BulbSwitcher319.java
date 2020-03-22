2dc20908e246c064ee20c254b40daf95640c3eb4
pengfei.zhao <pengfei.zhao@163.com>
2020-03-22 18:38:36 +0800

    数学技巧解决灯泡问题

---
+++ DSAndAlgorithm/src/main/java/com/javabase/dsandalgo/tree/binary/leetcode/random/medium/BulbSwitcher319.java
@@ -0,0 +1,12 @@
+package com.javabase.dsandalgo.tree.binary.leetcode.random.medium;
+
+public class BulbSwitcher319 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int bulbSwitch(int n) {
+        return  Double.valueOf(Math.sqrt(n + 0.0)).intValue();
+    }
+}
